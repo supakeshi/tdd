289754c17d5b3689a99be31661bae570cb276fc2
Supakeshi <supakeshi@gmail.com>
2024-02-12 21:13:33 -0800

    more edits to counter.py per workflow.yaml

--- src/counter.py
+++ src/counter.py
@@ -5,36 +5,32 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
-# We will use the app decorator and create a route called slash counters.
-# specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
-# on this function is "POST".
-@app.route('/counters/<name>', methods=['POST'])
 
-def create_counter(name): 
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
-    if name in COUNTERS: 
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    if name in COUNTERS:
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
-@app.route('/counters/<name>', methods=['PUT'])         # Step 1
 
-def update_counter(name):                               # Step 2
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
     """Update a counter by incrementing it by 1"""
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
-    COUNTERS[name] += 1                                 # Step 3
-    return {name: COUNTERS[name]}, status.HTTP_200_OK   # Step 4
+    COUNTERS[name] += 1
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
 
 @app.route('/counters/<name>', methods=['GET'])
-
 def read_counter(name):
     """Read a counter"""
     app.logger.info(f"Request to read counter: {name}")
     global COUNTERS
-    if name not in COUNTERS: 
-        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
